30b4c1b8fd506e87141daa2d98e6f9050804e362
Sahil Bhasin <119886591+TwinklePie22@users.noreply.github.com>
2024-01-24 10:20:31 +0530

    Update Warshalls_Algo.cpp

--- Warshalls_Algo.cpp
+++ Warshalls_Algo.cpp
@@ -6,21 +6,21 @@ using namespace std;
 void Warshall(vector<vector<int>>& graph) {
     int n = graph.size();
 
-    vector<vector<int>> dist(n, vector<int>(n, 0));
+    vector<vector<int>> clousre(n, vector<int>(n, 0));
 
     for (int i = 0; i < n; ++i)
         for (int j = 0; j < n; ++j)
-            dist[i][j] = graph[i][j];
+            clousre[i][j] = graph[i][j];
 
     for (int k = 0; k < n; ++k)
         for (int i = 0; i < n; ++i)
             for (int j = 0; j < n; ++j)
-                dist[i][j] = dist[i][j] || (dist[i][k] && dist[k][j]);
+                clousre[i][j] = clousre[i][j] || (clousre[i][k] && clousre[k][j]);
 
-    cout << "Transitive dist Matrix:\n";
+    cout << "Transitive clousre Matrix:\n";
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            cout << dist[i][j] << "\t";
+            cout << clousre[i][j] << "\t";
         }
         cout << endl;
     }
