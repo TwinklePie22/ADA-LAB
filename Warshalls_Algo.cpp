b7faeec714a63cb7263c6eb3cc1882d046c9cae1
Sahil Bhasin <119886591+TwinklePie22@users.noreply.github.com>
2024-03-14 00:54:29 +0530

    Update Warshalls_Algo.cpp

--- Warshalls_Algo.cpp
+++ Warshalls_Algo.cpp
@@ -1,4 +1,32 @@
 #include <iostream>
+using namespace std;
+
+void warshall(int a[20][20], int n) {
+    for (int k = 0; k < n; k++)
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                a[i][j] = (a[i][j] || a[i][k] && a[k][j]);
+}
+
+int main() {
+    int n, a[20][20];
+    cout << "Enter number of nodes in the graph: ";
+    cin >> n;
+    cout << "Enter the adjacency matrix for the graph: \n";
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> a[i][j];
+    warshall(a, n);
+    cout << "\nThe path matrix is: \n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++)
+            cout << a[i][j] <<"\t";
+        cout << endl;
+    }
+    return 0;
+}
+/*
+#include <iostream>
 #include <vector>
 
 using namespace std;
@@ -35,3 +63,4 @@ int main() {
     Warshall(graph);
     return 0;
 }
+*/
