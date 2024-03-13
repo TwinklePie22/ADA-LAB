b1e3ead40f08ee5efc4819f1746feb1870768312
Sahil Bhasin <119886591+TwinklePie22@users.noreply.github.com>
2024-03-14 04:08:40 +0530

    Update NQueen.cpp

--- NQueen.cpp
+++ NQueen.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include<ctime>
 using namespace std;
 class Solution {
 public:
@@ -8,7 +9,7 @@ public:
       vector<bool> cols(n);
       vector<bool> diag1(2 * n - 1);
       vector<bool> diag2(2 * n - 1);
-      vector<string> board(n, string(n, '.'));
+      vector<string> board(n, string(n, '*'));
       dfs(n, 0, cols, diag1, diag2, board, ans);
       return ans;
     }
@@ -25,20 +26,27 @@ private:
             cols[j] = diag1[i + j] = diag2[j - i + n - 1] = true;
             dfs(n, i + 1, cols, diag1, diag2, board, ans);
             cols[j] = diag1[i + j] = diag2[j - i + n - 1] = false;
-            board[i][j] = '.';
+            board[i][j] = '*';
         }
     }
 };
 int main() {
     Solution solution;
-    int n = 4; 
+    int n;
+    cout << "Enter the number of queens: ";
+    cin>> n;
+    clock_t start = clock();
     vector<vector<string>> result = solution.solveNQueens(n);
 
+    cout<<"Number of solutions: \n";
     for (const auto& solution : result) {
         for (const auto& row : solution) {
-            cout << row  << endl;
+            cout << row <<" " << endl;
         }
         cout<<"\n";
     }
+    clock_t end = clock();
+    double time_spent = double(end - start) / CLOCKS_PER_SEC * 1000.0; // Convert to milliseconds
+    cout << "Time spent: " << time_spent << " ms";
     return 0;
 }
