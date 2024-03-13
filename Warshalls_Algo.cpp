25643163ea44826ce492629f7a0a5d30b8af1b77
Sahil Bhasin <119886591+TwinklePie22@users.noreply.github.com>
2024-03-14 01:03:14 +0530

    Update Warshalls_Algo.cpp

--- Warshalls_Algo.cpp
+++ Warshalls_Algo.cpp
@@ -1,28 +1,31 @@
 #include <iostream>
 using namespace std;
 
-void warshall(int a[20][20], int n) {
+void Warshall(int graph[20][20], int n) 
+{
     for (int k = 0; k < n; k++)
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
-                a[i][j] = (a[i][j] || a[i][k] && a[k][j]);
-}
+                graph[i][j] = (graph[i][j] || (graph[i][k] && graph[k][j]));
 
-int main() {
-    int n, a[20][20];
-    cout << "Enter number of nodes in the graph: ";
-    cin >> n;
-    cout << "Enter the adjacency matrix for the graph: \n";
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            cin >> a[i][j];
-    warshall(a, n);
-    cout << "\nThe path matrix is: \n";
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++)
-            cout << a[i][j] <<"\t";
+    cout << "Transitive Closure Matrix:\n";
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << graph[i][j] << "\t";
+        }
         cout << endl;
     }
+}
+int main() {
+    int n, graph[20][20];
+    cout << "Enter the number of nodes: ";
+    cin >> n;
+
+    cout << "Enter the adjacency matrix:\n";
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+            cin >> graph[i][j];
+    Warshall(graph, n);
     return 0;
 }
 /*
