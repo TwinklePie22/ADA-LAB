4e04602bfad50dff1593683a053ceecb1f3ee36e
Sahil Bhasin <119886591+TwinklePie22@users.noreply.github.com>
2024-03-13 20:30:19 +0530

    Update quickSort.cpp

--- quickSort.cpp
+++ quickSort.cpp
@@ -3,49 +3,49 @@
 #include<iostream>
 using namespace std;
 
-int divideArray(int arr[], int lb, int ub)
+int divideArray (int arr[], int lb, int ub)   // lb= lower bound and ub= upper bound
 {
-    int pivot = arr[ub]; // Use the last element as the pivot
-    int i = lb - 1;
-    for (int j = lb; j < ub; j++)
-    {
-        if (arr[j] < pivot)
-        {
-            i++;
-            swap(arr[i], arr[j]);
-        }
-    }
-    swap(arr[i + 1], arr[ub]); 
-    return i + 1;
+	int pivot= arr[lb];
+	int i = lb;
+	for(int j=i+1; j<=ub; j++)
+	{
+		if(arr[j] < pivot)
+		{
+			i++;
+			swap(arr[i],arr[j]);
+		}
+	}
+	swap(arr[lb],arr[i]);	
+	return i;
 }
-void quickSort(int arr[], int lb, int ub)
+void quickSort (int arr[], int lb, int ub)
 {
-    if (lb < ub)
-    {
-        int p = divideArray(arr, lb, ub);
-        quickSort(arr, lb, p - 1);
-        quickSort(arr, p + 1, ub);
-    }
+	if(lb < ub)
+	{
+		int p= divideArray(arr, lb, ub );
+		quickSort(arr, lb, p-1);
+		quickSort(arr, p+1, ub);
+	}
 }
 void printArray(int arr[], int size)
 {
-    for (int a = 0; a < size; a++)
-    {
-        cout << arr[a] << " ";
-    }
-    cout << endl;
-}
-int main()
-{
-    int arr[] = {5, 3, 1, 9, 8, 2, 4, 7};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    
-    cout << "Original Array: ";
-    printArray(arr, n);
-    quickSort(arr, 0, n - 1);
-    cout << "Sorted Array: ";
-    printArray(arr, n);
-    
-    return 0;
+	for(int a=0; a<size; a++)
+	{
+		cout<<arr[a]<<" " ;
+	}
+	cout<<endl;
 }
 
+int main()
+{
+	int arr[]= {5, 3, 1, 9, 8, 2, 4, 7};
+	int n= sizeof(arr)/sizeof(arr[0]);
+	cout<< "Original Array: ";
+	printArray(arr,n);
+	
+	// Sorted array
+	quickSort(arr, 0, n-1);
+	cout<<"Sorted Array: ";
+	printArray(arr, n);
+	return 0;
+}
